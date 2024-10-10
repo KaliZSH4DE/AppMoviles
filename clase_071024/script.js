da22e75913c55947aa8fdfecb14d2556696b5c6d
ADAM <marquez.rodriguez.adam.jared@gmail.com>
2024-10-09 20:57:46 -0600

    Clase 09 10 2024

--- clase_071024/script.js
+++ clase_071024/script.js
@@ -1,24 +1,29 @@
+//Funcion para extraer los valores de los botones presionados
 function appendToDisplay(value){
-    document.getElementById('display').value += value;
+    document.getElementById('display').value += value; //Extrae el valor del button para mostrarlo en el id = display
 }
 
+//Funcion para eliminar todo el valor del id display
 function clearDisplay(){
-    document.getElementById('display').value = '';
+    document.getElementById('display').value = ''; //Cambia el valor del id display por " "
 }
 
+//Funcion para eliminar el ultimo digito
 function deleteLast(){
-    let displayValue = document.getElementById('display').value;
-    document.getElementById('display').value = displayValue.slice(0, -1);
+    let displayValue = document.getElementById('display').value; //Extrae el valor del id display y lo guarda en la variable displayValue
+    document.getElementById('display').value = displayValue.slice(0, -1); //Cambia el valor del display y elimina el ultimo digito escrito
 }
 
-function calculateResult(){
+//Funcion para calcular el resultado
+function calculateResult(){ 
+    //Usa el try catch para el manejo de errores
     try{
-        let result = eval(document.getElementById('display').value);
-        document.getElementById('display').value = result;
+        let result = eval(document.getElementById('display').value); //Extrae todo el valor del id display y lo guarda en una variable result donde se ejecutan las opercaciones
+        document.getElementById('display').value = result; //Cambia el valor del id display por el resultado de las operaciones guardadas en la variable result
 
 
     } catch(error){
-        alert('Expresion invalida');
+        alert('Expresion invalida'); //Manda una alerta del navegador con el texto "Expresion invalida" en caso de que haya un error
     }
 
 }
